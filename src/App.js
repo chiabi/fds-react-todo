c7688ac0e65ee8dc8b1ccb7301564abeeebd4a90
chiabi <chiabi88@gmail.com>
2018-06-07 15:28:11 +0900

    할일 목록 구현 시작

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+const todos = [
+  {
+    id: 1,
+    body: 'React 공부',
+    complete: true,
+  },
+  {
+    id: 2,
+    body: 'Redux 공부',
+    complete: false,
+  }
+]
+class App extends Component {
+  state = {todos}
+  render() {
+    const todos = this.state.todos;
+    return (
+      <div>
+        <h1>할 일 목록</h1>
+        <ul>
+          {
+            todos.map(todo => (
+              <li key={todo.id}>{todo.body}</li>
+            ))
+          }
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
