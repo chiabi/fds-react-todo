6f19952a9b1cb86786bdd6b5e697be6ab23cf714
chiabi <chiabi88@gmail.com>
2018-06-18 12:39:06 +0900

    Modify with uncontrolled components

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,37 +3,34 @@ import React from 'react'
 import todoAPI from '../todoAPI'
 
 export default class LoginForm extends React.Component {
-  state = {
-    username: '',
-    password: ''
+  // usernameRef = React.createRef();
+  // passwordRef = React.createRef();
+
+  // handleLoginClick = e => {
+  //   const {onLogin} = this.props;
+  //   onLogin(this.usernameRef.current.value, this.passwordRef.current.value);
+  // }
+
+  handleLoginSubmit = async e => {
+    const username = e.target.elements.username.value; 
+    const password = e.target.elements.password.value;
+    const {onLogin} = this.props;
+    onLogin(username, password);
+    e.preventDefault();
   }
-  handleUsernameChange = e => {
-    this.setState({
-      username: e.target.value
-    })
-  }
-  handlePasswordChange = e => {
-    this.setState({
-      password: e.target.value
-    })
-  }
-  handleLoginClick = async e => {
-    const {onLogin} = this.props
-    onLogin(this.state.username, this.state.password)
-  }
+
   render() {
-    const {username, password} = this.state
     return (
-      <React.Fragment>
+      <form onSubmit={this.handleLoginSubmit}>
         <h1>로그인</h1>
         <label>
-          아이디: <input type="text" value={username} placeholder="username" onChange={this.handleUsernameChange} />
+          아이디: <input type="text" ref={this.usernameRef} name="username" />
         </label>
         <label>
-          비밀번호: <input type="password" value={password} placeholder="password" onChange={this.handlePasswordChange}/>
+          비밀번호: <input type="password" ref={this.passwordRef} name="password" />
         </label>
-        <button onClick={this.handleLoginClick}>로그인</button>
-      </React.Fragment>
+        <button>로그인</button>
+      </form>
     )
   }
 }
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -8,17 +8,25 @@ class UserProvider extends React.Component {
 
   login = async (username, password) => {
     // 로그인 요청
-    const payload = {
-      username,
-      password
+    try {
+      const res = await todoAPI.post('/users/login', {
+        username: username,
+        password: password
+      })
+  
+      // localStorage에 토큰 저장
+      localStorage.setItem('token', res.data.token)
+  
+      // 페이지 전환
+      this.props.onLogin();
+    } catch(e) {
+      // e.response이면 axios의 에러일 것이라는 가정을 할 수 있다.
+      if (e.response) {
+        if (e.response && e.response.status === 400) {
+          alert('아이디 혹은 비밀번호가 일치하지 않습니다.');
+        }
+      }
     }
-    const res = await todoAPI.post('/users/login', payload)
-
-    // localStorage에 토큰 저장
-    localStorage.setItem('token', res.data.token)
-
-    // 페이지 전환
-    this.props.onLogin()
   }
 
   render() {
@@ -26,7 +34,9 @@ class UserProvider extends React.Component {
       login: this.login
     }
     return (
-      <Provider value={value}>{this.props.children}</Provider>
+      <Provider value={value}>
+        {this.props.children}
+      </Provider>
     )
   }
 }
