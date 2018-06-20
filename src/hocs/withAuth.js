592a062e5364a1b499d68b3d6bf1d410c6db48e5
chiabi <chiabi88@gmail.com>
2018-06-20 12:04:42 +0900

    Add withauth

---
+++ src/hocs/withAuth.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {Redirect} from 'react-router-dom';
+
+export default function withAuth(WrappedCompoennet){
+  return class extends React.Component {
+    render() {
+      if(localStorage.getItem('token')) { 
+        return <WrappedCompoennet {...this.props}/>;
+      } else {
+        return <Redirect to="/login"/>;
+      }
+    }
+  }
+}
